e00e3741f9257464b11bcbe686c3661a8fa923c4
andresslacson1989 <48279587+andresslacson1989@users.noreply.github.com>
2022-12-23 10:25:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,21 +3,29 @@ if ! [ $(id -u) = 0 ]; then
    echo "I am not root!"
    exit 1
 fi
+
 echo Installing Openvpn
-sleep 2
+sleep 1
 echo Installing Squid
-sleep 2
+sleep 1
 echo Installing CertBot
 sleep 1
 echo Installing Openvpn
-sleep 2
+sleep 1
 echo Installing Squid
-sleep 2
+sleep 1
 echo Installing CertBot
-sleep 1echo Installing Openvpn
-sleep 2
+sleep 1
+echo Installing Openvpn
+sleep 1
 echo Installing Squid
-sleep 2
+sleep 1
+echo Installing CertBot
+sleep 1
+echo Installing Openvpn
+sleep 1
+echo Installing Squid
+sleep 1
 echo Installing CertBot
 sleep 1
 echo Done
